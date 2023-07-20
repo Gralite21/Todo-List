5078b5355432c6253f3b2e5ca5b59812a0e80a89
Tim Lagier <tim.lagier33@gmail.com>
2023-07-20 21:41:12 +0200

    update

--- todo.js
+++ todo.js
@@ -1,9 +1,26 @@
 const todoList=[];
 
+renderTodoList();
+
+function renderTodoList(){
+    let todoListHtml='';
+
+    for (let i=0;i< todoList.length;i++){
+        const toDO = todoList[i];
+        const html = `<p>${toDO}</p>`
+        todoListHtml += html;
+    }
+    console.log(todoListHtml);
+
+    document.querySelector('.displayList').innerHTML=todoListHtml;
+}
+
 function addTodo(){
     const inputElement =document.querySelector('.todoInput');
     const name=inputElement.value
 
     todoList.push(name)
+    console.log(todoList)
     inputElement.value='';
+    renderTodoList();
 }
